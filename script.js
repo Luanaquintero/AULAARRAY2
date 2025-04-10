45bbd5c57dd9f0f5b8fed296ab778c66780ad351
Luanaquintero <luanaquintero6@gmail.com>
2025-04-10 09:35:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//JS - JavaScript
+
+var nomes = ["Alice", "Raquel", "Pedro", "Gabriela", "Priscila","Kali", "Rafael", "Leticia","Julio", "Madalena", "João", "Antônio","Beatriz", "Giovanna", "Evelyn", "Paula", "Jonas",
+ "Fernanda", "Claudia", "Gustavo", "Laura", "Nicoly", "André", "Mauricio", "Bruno", "Vitor", "Guilherme", "Camila", "Elvis","Eduardo", "Monica"]
+var nomeescolhido = parseInt(prompt("Digite um número de 0 a 30"))
+
+document.write ("Nome escolhido:" +     nomes[nomeescolhido])
