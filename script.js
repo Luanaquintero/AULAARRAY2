13e23ded8c209bbecab3d1b58b767614f8dcdbff
Luanaquintero <luanaquintero6@gmail.com>
2025-04-10 09:34:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//JS - JavaScript
+
+var linhas = ["Anne", "Cléa", "Duna", "Bella", "Camila", "Esterlina", "Charme", "Barroco Multicolor", "Fiore", "Maxi Mouline", "Amigurumi", "Paris", "Esterlina 5", "Camila 1000", "Cisne D’Primera", "Cristallo"]
+
+var linhaescolhida = parseInt(prompt("Digite um número de 0 a 14"))
+
+document.write ("Linha escolhida: " +     linhas[linhaescolhida])
